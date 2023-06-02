a48c38e05c6c9bc7b7bb4aaa863d10c7985db47e
joekariuki3 <joekariuki3@gmail.com>
2023-06-02 14:04:14 +0300

    mathematics operations
    operations done in mathematics

---
+++ ex3.py
@@ -0,0 +1,31 @@
+#show what i will not do
+print("I will not count my chickens:")
+
+#shows number of Hens
+print("Hens", 25 + 30 / 6)
+
+#shows number of Roosters
+print("Roosters", 100 -25 * 3 % 4)
+
+#show what to do 
+print("Now i will count the eggs:")
+
+#add number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#check if 3 + 2 is less than 5 - 7
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#check if 3 + 2 is less than 5 - 7
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
