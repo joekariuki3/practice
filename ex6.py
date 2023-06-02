2056c6cd6c05b078f1693fb5aeeae6cdcfd954da
joekariuki3 <joekariuki3@gmail.com>
2023-06-02 16:53:25 +0300

    String and naming
    using formating more on strings

---
+++ ex6.py
@@ -0,0 +1,34 @@
+#Assign value 10 to variable types_of_people
+types_of_people = 10
+
+#Assign a formated string to variable x
+x = f"There are {types_of_people} types of people."
+
+#Assing a sting to variable
+binary = "binary"
+
+#Assign a string to variablr
+do_not = "don't"
+
+#Assign a formatted string to variable y
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+#Assign boolean to variable
+hilarious = False
+
+#Assign formated-empty string to a variable
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of ..."
+e = "a string with a right side."
+
+#concatinate two strings
+print(w + e)
