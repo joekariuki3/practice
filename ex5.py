ade3efdb88618a088a95ea2fcc840dd62ff2d113
joekariuki3 <joekariuki3@gmail.com>
2023-06-02 16:12:46 +0300

    More variables and naming
    using the formated string to be able to print out variabled together with strings

---
+++ ex5.py
@@ -0,0 +1,20 @@
+name = 'Joel K. Muhoho'
+age = 28 # not a lie
+height = 74 # inches
+heightInCm = round(height * 2.54) # cm
+weight = 120 # pounds
+weightInKgs = round(weight * 0.453592) # kgs
+eyes = 'Black'
+teeth = 'white'
+hair = 'Black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches or {heightInCm} cm  tall")
+print(f"He's {weight} pounds or {weightInKgs} kgs heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee")
+
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} i get {total}")
