0cf00604675e802871f769255c6f9586c46b249b
joekariuki3 <joekariuki3@gmail.com>
2023-06-03 14:42:15 +0300

    use of argv and input function
    take arguments from user
    also request input from user when the script is running

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv,path
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("This is PATH: ",path)
+print("My name is: ",input("Whats your name? "))
