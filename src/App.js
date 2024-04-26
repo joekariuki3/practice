3636cc50897032dd2ab893bd976fa392305e9e2b
joekariuki3 <joekariuki3@gmail.com>
2024-04-26 18:17:11 +0300

    added the signUp and SignIn components to app.js and i created routes for them. when you type signUp/ or signIn/ then you are redirected to the pages respectively

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectsDetails from "./components/projects/ProjectsDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/project/:id" element={<ProjectsDetails />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
         </Routes>
       </div>
     </BrowserRouter>
