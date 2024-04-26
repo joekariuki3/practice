4ba6c3ba628d555423a81215ca808bddea3687f2
joekariuki3 <joekariuki3@gmail.com>
2024-04-27 00:12:31 +0300

    instaled redux react-redux and @reduxjs. i also created the redux store and used the rootReducer with it. i also wrapped the App component with provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+const store = configureStore(rootReducer);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <provider store={store}>
+      <App />
+    </provider>
   </React.StrictMode>
 );
 
